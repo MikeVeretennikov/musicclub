11ccb0ba7211501c31d648c0cc771e24943bd622
Igor <igor.mamaev1@gmail.com>
2025-11-13 18:19:47 +0300

    Добавлена возможность делать объявления в админ панельку

--- bot/handlers/adminpanel.py
+++ bot/handlers/adminpanel.py
@@ -30,6 +30,21 @@ async def admin_panel_getter(dialog_manager: DialogManager, event_from_user: Use
         await dialog_manager.done()
     return {}
 
+async def on_announcement(message: Message, msg_input: MessageInput, manager: DialogManager):
+    async with get_db_session() as session:
+        users: list[Person] = (await session.execute(select(Person))).scalars().all()
+
+    counter = 0
+    for user in users:
+        try:
+            await message.copy_to(user.id)
+            counter += 1
+        except:
+            continue
+
+    await message.reply(text=f"Отправил сообщение {counter} пользователям. Это {(counter / len(users)) * 100}% базы данных, в ней всего {len(users)}")
+    await manager.switch_to(AdminPanel.menu)
+
 router.include_router(
     Dialog(
         Window(
@@ -41,9 +56,20 @@ router.include_router(
                     CreateEvent.title, data={"started_id": c.from_user.id}
                 ),
             ),
+            Button(
+                Const("Сделать объявление"),
+                id="announcement",
+                on_click=lambda c, b, m: m.switch_to(AdminPanel.announcement)
+            ),
             Cancel(Const("Назад")),
             getter=admin_panel_getter,
             state=AdminPanel.menu,
+        ),
+        Window(
+            Const("Сделать объявление: отправь мне сообщение и я разошлю его всем кого знаю"),
+            MessageInput(func=on_announcement),
+            getter=admin_panel_getter,
+            state=AdminPanel.announcement,
         )
     )
 )
--- bot/states/adminpanel.py
+++ bot/states/adminpanel.py
@@ -3,3 +3,4 @@ from aiogram.fsm.state import StatesGroup, State
 
 class AdminPanel(StatesGroup):
     menu = State()
+    announcement = State()
