219a6f1353c48335c15845f2f6d37899142306b5
Igor <igor.mamaev1@gmail.com>
2025-12-22 14:38:58 +0300

    auth.proto

--- server/src/services/claims.rs
+++ server/src/services/claims.rs
@@ -2,7 +2,10 @@ use serde::{Deserialize, Serialize};
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct Claims {
-    pub sub: u64,
+    pub tg_id: u64,
+    // unix time when the form was opened
+    pub auth_date: u64,
+
     pub exp: usize,
     pub iat: usize,
     pub is_admin: bool,
